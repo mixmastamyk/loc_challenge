5b6f93cd57a2026ae7ac3e2d26c09094f157b481
Gringo Suave <github@mgmiller.net>
2018-02-20 15:53:54 -0800

    copy input to results page for testing.

--- challenge.py
+++ challenge.py
@@ -10,19 +10,25 @@ from .patentSimilarityApp_sim import get_input, full_pipeline
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=('GET', 'POST'))
 def start_page():
 
     if request.method == 'POST':
 
-        return redirect(url_for('results_page'))
+        input_text = request.form.get('input_text')
+        # temporary redirect with data for testing purposes
+        return redirect(url_for('results_page', input_text=input_text), code=307)
     else:
         return render('start.html')
 
 
-@app.route('/results/')
-def results_page(name=None):
+@app.route('/results/', methods=('GET', 'POST'))
+def results_page():
 
-    results = full_pipeline(get_input('foo bar baz '))
+    results = []
+
+    if request.method == 'POST':
+        input_text = request.form.get('input_text')
+        results = full_pipeline(input_text, title=input_text)
 
     return render('results.html', results=results)
--- patentSimilarityApp_sim.py
+++ patentSimilarityApp_sim.py
@@ -10,10 +10,10 @@ def get_input(input_text):
     return input_text
 
 
-def full_pipeline(df):
+def full_pipeline(df, title='Document Title 1'):
 
     results = [
-        ('Document Number 1', 'Document Title 1'),
+        ('Document Number 1', title),
         ('Document Number 2', 'Document Title 2'),
         ('Document Number 3', 'Document Title 3'),
         ('Document Number 4', 'Document Title 4'),
